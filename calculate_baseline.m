998d598395dbfebc7f43a3091ca5ccf7e711b81b
GeoISPy <46223431+GeoISPy@users.noreply.github.com>
2025-03-31 14:17:26 +0800

    Add files via upload

---
+++ calculate_baseline.m
@@ -0,0 +1,18 @@
+function []=calculate_baseline(year,month,month_day,station)
+data=download_supermag(year,month,month_day,station);
+% load(['E:\DATA\MAG\Ground\Ground_main\',station,'\',num2str(year),num2str(month,'%02d'),'.mat']);
+extracted_data=extract_q_values('E:\DATA\INDEX\Quiet_days.txt');
+[ia,~]=find(extracted_data(:,1)==year&extracted_data(:,2)==month);
+q_date=extracted_data(ia,3:7);
+[ia,~]=find(data(:,3)==q_date(1)|data(:,3)==q_date(2)|data(:,3)==q_date(3)|data(:,3)==q_date(4)|data(:,3)==q_date(5));
+q_data=data(ia,:);
+lt=datevec(datetime(q_data(:,1:6))+minutes(q_data(1,8)));
+lt=lt(:,4)+lt(:,5)/60+lt(:,6)/3600;
+[ia,~]=find(lt>=22&lt<=4);
+baseline=mean(q_data(ia,9));
+data_rb=data(:,9)-baseline;
+data=[data,data_rb];
+save(['E:\DATA\MAG\Ground\Ground_main\',station,'\',num2str(year),num2str(month,'%02d'),'_rb.mat'],'data')
+
+
+
---
+++ download_supermag.m
@@ -0,0 +1,27 @@
+function data=download_supermag(year,month,month_day,station)
+
+data=[];
+for i=1:month_day
+    sm_data = fetchSuperMAG('data','gispyer',[year,month,i,0,0,0],86400,'all,baseline=none',station);
+    if ischar(sm_data)
+        sm_data=regexprep(sm_data, 'nan', 'null');
+        index=find(sm_data=='[',1);
+        sm_data=jsondecode(sm_data(index:end-26));
+    end
+    TVAL=[sm_data.tval];
+    tval=datevec(datetime(datenum(datetime(1970,1,1,0,0,0))+TVAL/86400, 'ConvertFrom', 'datenum').');
+    N_NEZ = arrayfun(@(k) sm_data(k).N.nez, 1:numel(sm_data));
+    E_NEZ = arrayfun(@(k) sm_data(k).E.nez, 1:numel(sm_data));
+    Z_NEZ = arrayfun(@(k) sm_data(k).Z.nez, 1:numel(sm_data));
+    MLT=[sm_data.mlt];
+    MCOLAT=[sm_data.mcolat];
+    GLON=[sm_data.glon];
+    GLAT=[sm_data.glat];
+    SZA=[sm_data.sza];
+    data=[data;tval,GLAT.',GLON.',N_NEZ.',E_NEZ.'];
+end
+path=['E:\DATA\MAG\Ground\Ground_main\',station,'\'];
+if ~exist(path,"dir")
+    mkdir(path)
+end
+save(['E:\DATA\MAG\Ground\Ground_main\',station,'\',num2str(year),num2str(month,'%02d'),'.mat'],'data')
