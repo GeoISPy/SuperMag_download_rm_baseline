8f7bbaad08f79e54521933028b8229f88f1b1107
GeoISPy <46223431+GeoISPy@users.noreply.github.com>
2025-11-20 14:36:21 +0800

    Update calculate_baseline.m

--- calculate_baseline.m
+++ calculate_baseline.m
@@ -1,18 +1,20 @@
-function []=calculate_baseline(year,month,month_day,station,usrid)
+function []=calculate_download_baseline(year,month,month_day,station,usrid)
 data=download_supermag(year,month,month_day,station,usrid);
 % load(['E:\DATA\MAG\Ground\Ground_main\',station,'\',num2str(year),num2str(month,'%02d'),'.mat']);
-extracted_data=extract_q_values('E:\DATA\INDEX\Quiet_days.txt');
+extracted_data=extract_q_values('E:\Data\Index\Quiet_days.txt');
 [ia,~]=find(extracted_data(:,1)==year&extracted_data(:,2)==month);
 q_date=extracted_data(ia,3:7);
 [ia,~]=find(data(:,3)==q_date(1)|data(:,3)==q_date(2)|data(:,3)==q_date(3)|data(:,3)==q_date(4)|data(:,3)==q_date(5));
+data(data(:,9)==999999,9)=nan;
 q_data=data(ia,:);
-lt=datevec(datetime(q_data(:,1:6))+minutes(q_data(1,8)));
+lt=datevec(datetime(q_data(:,1:6))+minutes(q_data(1,8)*4));
 lt=lt(:,4)+lt(:,5)/60+lt(:,6)/3600;
-[ia,~]=find(lt>=22&lt<=4);
-baseline=mean(q_data(ia,9));
+[ia,~]=find(lt>=22|lt<=2);
+baseline=nanmean(q_data(ia,9));
 data_rb=data(:,9)-baseline;
 data=[data,data_rb];
-save(['E:\DATA\MAG\Ground\Ground_main\',station,'\',num2str(year),num2str(month,'%02d'),'_rb.mat'],'data')
+save(['E:\Data\Ground_main\',station,'\',num2str(year),num2str(month,'%02d'),'_rb.mat'],'data')
+
 
 
 
